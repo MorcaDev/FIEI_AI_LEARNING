e901a45519503a4fbec24e5301c0b5786c791292
MorcaDesktop <morca225@gmail.com>
2025-10-23 21:36:44 -0500

    V1.2.0 Security

--- Core/settings.py
+++ Core/settings.py
@@ -121,6 +121,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+DEBUG = 'RENDER' not in os.environ
+
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
