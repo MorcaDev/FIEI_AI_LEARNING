ed8cae6959b247bb8d24d4dd23aca5a0f97a9b40
MorcaDesktop <morca225@gmail.com>
2025-10-23 21:18:28 -0500

    V1.1.6 Static

--- Core/settings.py
+++ Core/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "staticfiles"
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
