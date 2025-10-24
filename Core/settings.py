7908077c6e7cd49096c4db12feac60dcf8e2dc8a
MorcaDesktop <morca225@gmail.com>
2025-10-23 23:50:48 -0500

    V1.2.3 debug

--- Core/settings.py
+++ Core/settings.py
@@ -17,7 +17,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -25,13 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dnp3*m-3fk*2i3$lq*1_1!@fjrm1#ynja_ahca*rgocvu1bxj!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,8 +117,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
